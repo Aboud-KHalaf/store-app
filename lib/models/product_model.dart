6467b2a8508e5c8dec775825e8d2eedab7c79cd5
unknown <aboudkalaf10@gmail.com>
2024-03-06 13:50:45 +0300

    create product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,18 @@
+class ProductModel {
+  final String productId,
+      productTitle,
+      productPrice,
+      productCategory,
+      productDescription,
+      productImage,
+      productQuantity;
+
+  ProductModel(
+      {required this.productId,
+      required this.productTitle,
+      required this.productPrice,
+      required this.productCategory,
+      required this.productDescription,
+      required this.productImage,
+      required this.productQuantity});
+}
